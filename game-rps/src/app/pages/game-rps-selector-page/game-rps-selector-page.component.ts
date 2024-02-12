bb64ceef8eaacd7bf971993ec84fcbaf73d9f446
FrancoDavid <franco.avalos@live.com>
2024-02-11 21:07:05 -0300

    feat: layout components selectors

--- game-rps/src/app/pages/game-rps-selector-page/game-rps-selector-page.component.ts
+++ game-rps/src/app/pages/game-rps-selector-page/game-rps-selector-page.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { GameType } from '../../types/game-type.type';
 import { GameRpsSelectorComponent } from '../../components/game-rps-selector/game-rps-selector.component';
@@ -14,17 +14,42 @@ import { GameRpsSelectorComponent } from '../../components/game-rps-selector/gam
         GameRpsSelectorComponent
     ],
     template: `
-        <game-rps-selector></game-rps-selector>
+        <aside class="game-config-container" *ngIf="gameConfig.mode === 'normal'">
+            <section class="selector-contain--center">
+                <game-rps-selector [mode]="gameConfig.options[0]"></game-rps-selector>
+            </section>
+            <section class="selector-contain--between">
+                <game-rps-selector [mode]="gameConfig.options[1]"></game-rps-selector>
+                <game-rps-selector [mode]="gameConfig.options[2]"></game-rps-selector>
+            </section>
+        </aside>
     `,
     styleUrl: './game-rps-selector-page.component.css',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GameRpsSelectorPageComponent {
+export class GameRpsSelectorPageComponent implements OnInit {
 
-    private _gameType: GameType;
+    public gameConfig: {
+        options: Array<'paper' | 'rock' | 'scissor'>;
+        mode: GameType
+    };
 
     constructor(private _router: ActivatedRoute) {
-        this._gameType =  (this._router.snapshot.paramMap.get('type') &&
-                           this._router.snapshot.paramMap.get('type') === 'sheldon' ) ? 'sheldon' :'normal';
+        this.gameConfig = {
+            options: [],
+            mode:   (this._router.snapshot.paramMap.get('type') &&
+                    this._router.snapshot.paramMap.get('type') === 'sheldon') ? 'sheldon' : 'normal'
+        };
     }
+
+    ngOnInit(): void {
+        if (this.gameConfig.mode === 'normal') {
+            this.gameConfig.options = ['paper', 'rock', 'scissor'];
+        } else {
+            this.gameConfig.options = [];
+        }
+    }
+
+
+
 }
